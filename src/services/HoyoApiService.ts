ba7d177c8ce875dd1aa12c553ae43e80253dfa78
Vianney Carbel-Zekpa <hyperiya.vcz@outlook.com>
2025-03-13 18:11:55 -0400

    hoyo o clock

---
+++ src/services/HoyoApiService.ts
@@ -0,0 +1,19 @@
+// https://bbs-api-os.hoyolab.com/game_record/card/wapi/getGameRecordCard?uid=93112092 (all games mini-details)
+
+// HSR
+// https://sg-public-api.hoyolab.com/event/game_record/hkrpg/api/index?server={server}&role_id={roleid} (HSR Details)
+// https://sg-public-api.hoyolab.com/event/game_record/hkrpg/api/note?server=prod_official_usa&role_id=615855534 (HSR Battery Charge)
+// https://sg-public-api.hoyolab.com/event/game_record/hkrpg/api/challenge?schedule_type=1&server=prod_official_usa&role_id=615855534&need_all=false (Something..?)
+// https://sg-public-api.hoyolab.com/event/game_record/hkrpg/api/challenge_story?schedule_type=1&server=prod_official_usa&role_id=615855534&need_all=false (also something?)
+// https://sg-public-api.hoyolab.com/event/game_record/hkrpg/api/challenge_boss?schedule_type=1&server=prod_official_usa&role_id=615855534&need_all=false (???)
+
+
+// GI
+// https://sg-public-api.hoyolab.com/event/game_record/genshin/api/index?avatar_list_type=0&server={server}&role_id={roleid} (Genshin Details?)
+
+// ZZZ
+// https://sg-public-api.hoyolab.com/event/game_record_zzz/api/zzz/index?server={server}&role_id={roleid} (ZZZ Details)
+// https://sg-public-api.hoyolab.com/event/game_record_zzz/api/zzz/note?server=prod_gf_us&role_id=1000278659 (ZZZ Battery Charge)
+// https://sg-public-api.hoyolab.com/event/game_record_zzz/api/zzz/mem_detail?uid=1000278659&region=prod_gf_us&schedule_type=1 (ZZZ Deadly Assault)
+// https://sg-public-api.hoyolab.com/event/game_record_zzz/api/zzz/challenge?server=prod_gf_us&role_id=1000278659&schedule_type=1 (ZZZ Shiyu (Only Last four?))
+// https://sg-public-api.hoyolab.com/event/game_record_zzz/api/zzz/abyss_abstract?server=prod_gf_us&role_id=1000278659 (ZZZ HZ)
