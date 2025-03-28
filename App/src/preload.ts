f9dd86b8985b1e91f9df830264967d897596f751
Hyperiya <hyperiya.vcz@outlook.com>
2025-03-27 23:05:24 -0400

    missed a thing

--- App/src/preload.ts
+++ App/src/preload.ts
@@ -18,34 +18,17 @@ contextBridge.exposeInMainWorld('electron', {
     ipcRenderer.on('discord-notification', (_, notification) => {
       callback(notification);
     });
-  },
-  saveDiscordTokens: (tokens: any) => {
-    localStorage.setItem('discord-tokens', JSON.stringify(tokens));
-  },
-  loadDiscordTokens: () => {
-    const tokens = localStorage.getItem('discord-tokens');
-    return tokens ? JSON.parse(tokens) : null;
-  },
+  }
 });
 
 contextBridge.exposeInMainWorld('discord', {
   connect: () => ipcRenderer.invoke('discord:connect'),
   disconnect: () => ipcRenderer.invoke('discord:disconnect'),
   onNotification: (callback: (notification: any) => void) => {
-    ipcRenderer.on('discord:notification', (_, notification) => callback(notification));
+      ipcRenderer.on('discord:notification', (_, notification) => callback(notification));
   },
   removeNotificationListener: () => {
-    ipcRenderer.removeAllListeners('discord:notification');
+      ipcRenderer.removeAllListeners('discord:notification');
   }
 });
 
-// Handle IPC messages
-ipcRenderer.on('save-discord-tokens', (_event, tokens) => {
-  localStorage.setItem('discord-tokens', JSON.stringify(tokens));
-});
-
-// Handle IPC invocations
-ipcRenderer.on('load-discord-tokens', () => {
-  const tokens = localStorage.getItem('discord-tokens');
-  return tokens ? JSON.parse(tokens) : null;
-});
