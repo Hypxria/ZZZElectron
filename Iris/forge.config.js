55ce9015df70411ac5ec3d2f7a3d7492af70298c
Vianney Carbel-Zekpa <hyperiya.vcz@outlook.com>
2025-04-21 23:10:40 -0400

    ddd

--- Iris/forge.config.js
+++ Iris/forge.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   packagerConfig: {
     derefSymlinks: true,
-    asar: false,
+    asar: true,
     extraResource: [
       'src/assets/extension'
     ],
